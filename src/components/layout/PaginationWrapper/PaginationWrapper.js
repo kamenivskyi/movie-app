1be5f3d4f8857b9b0876c920980b7578155311f6
Roman Kamenivskyi <jumji19992412@gmail.com>
2020-02-25 14:01:39 +0200

    deleted  previous react-js-pagination and replaced with easier react-pagination-js

--- src/components/layout/PaginationWrapper/PaginationWrapper.js
+++ src/components/layout/PaginationWrapper/PaginationWrapper.js
@@ -1,51 +1,21 @@
 import React from 'react';
-// import Pagination from 'react-pagination-js';
-import 'react-pagination-js/dist/styles.css';
-// import Pagination from 'rc-pagination';
-import Pagination from 'react-js-pagination';
 import PropTypes from 'prop-types';
+import Pagination from 'react-pagination-js';
 
-const PaginationWrapper = props => {
-  const { currentPage, totalItems, totalPages, onChange } = props;
-  return (
-    <>
-      {totalPages > 1 ? (
-        <div className='pagination-container'>
-          {/* <Pagination
-            currentPage={currentPage}
-            totalSize={totalItems}
-            sizePerPage={20}
-            changeCurrentPage={onChange}
-            theme='circle'
-          /> */}
+import 'react-pagination-js/dist/styles.css';
 
-          {/* <Pagination
-            current={currentPage}
-            pageSize={20}
-            total={totalItems}
-            onChange={onChange}
-            nextIcon='next'
-            prevIcon='prev'
-            className='pagination'
-          /> */}
-          <Pagination
-            activePage={currentPage}
-            itemsCountPerPage={20}
-            totalItemsCount={totalItems}
-            pageRangeDisplayed={5}
-            onChange={onChange}
-            itemClass={'page-item'}
-            linkClass={'page-link'}
-            prevPageText={`Prev`}
-            nextPageText={`Next`}
-            firstPageText={`First`}
-            lastPageText={`Last`}
-          />
-        </div>
-      ) : null}
-    </>
-  );
-};
+const PaginationWrapper = ({ currentPage, totalItems, totalPages, onChange }) =>
+  totalPages > 1 ? (
+    <div className='pagination-container'>
+      <Pagination
+        currentPage={currentPage}
+        totalSize={totalItems}
+        sizePerPage={20}
+        changeCurrentPage={onChange}
+        theme='bootstrap'
+      />
+    </div>
+  ) : null;
 
 PaginationWrapper.propTypes = {
   currentPage: PropTypes.number,
